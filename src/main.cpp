34470d8f709d8708325e06a2a6b742da13e83d7a
tobiaswhughes <tobias.hughes@edisen.com>
2022-11-11 00:25:04 -0800

    Seperated arg read to different function

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,26 @@
 
 using namespace std;
 
+string getFileNameFromArgs(int, char **);
+
 int main(int argc, char **argv)
 {
-    string fileName;
-
-    if (argc <= 1)
-    {
-        cout << "Must provide ROM" << endl;
-        return 0;
-    }
-
-    fileName = argv[1];
+    string fileName = getFileNameFromArgs(argc, argv);
 
     Rom rom(fileName);
 
     rom.printTest();
 
     return 0;
+}
+
+string getFileNameFromArgs(int argc, char **argv)
+{
+    if (argc <= 1)
+    {
+        cout << "Must provide ROM" << endl;
+        exit(0);
+    }
+
+    return argv[1];
 }
